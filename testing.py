e30347736470897b5ba0773bd38023a8a02336aa
KanayBhandari <kanaybhandari2511@gmail.com>
2021-07-09 22:34:39 +0530

    script to convert csv to python dict

--- testing.py
+++ testing.py
@@ -1,51 +1,26 @@
-from pandas import read_csv
 import csv
+import os
 
-# with open('/home/kanay/projects/CSV_Files/institute.csv', encoding="latin-1") as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter='\t')
-#     l = []
-#     line_count = 0
- 
-#     for row in csv_reader:
-#         # if line_count < 4:
-#         # print(row)
-#         # print(row[-2])
-#         if row[-3] != '':
-#             l.append(row[-3])
-#         if row[-2] != '':
-#             l.append(row[-2])
-        
-#         line_count += 1
-#     l = set(l)
-#     l =list(l)
-#     # print(l)
-# l = [i.lower() for i in l]
-# print("index = ", l.index('no'))
-# # print(l)
-# d = {}
-# d["institutes"] = l
+filename = os.path.join(os.getcwd(), 'CSV_Files/institute.csv')
 
-# # print("dictionary = ", d)
-
-with open('/home/kanay/projects/CSV_Files/institute.csv', encoding="latin-1") as csv_file:
+with open(filename, encoding="latin-1") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter='\t')
     l = []
-    line_count = 0
- 
+
     for row in csv_reader:
-        # if line_count < 4:
-        # print(row)
-        # print(row[-2])
-        # if row[-3] != '':
-        #     l.append(row[-3])
+        if row[-3] != '':
+            l.append(row[-3])
         if row[-2] != '':
             l.append(row[-2])
-        
-        line_count += 1
+        if row[-1] != '':
+            l.append(row[-1])
+
     l = set(l)
-    l =list(l)
+    l = list(l)
+    # print(l)
+
 l = [i.lower() for i in l]
-# print("index = ", l.index('no'))
+
 d = {}
 d["institutes"] = l
 
